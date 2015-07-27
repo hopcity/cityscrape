da40a11738670233827f06d94f17ae756fd5dcc8
Dylan Raithel <dylanraithel@gmail.com>
2015-07-26 19:16:09 -0700

    Pep8 linter, did not run tests, need deps build automation

---
+++ grab_all_files.py
@@ -0,0 +1,52 @@
+'''
+    @author: clayton.young
+    deps: blah
+
+'''
+
+from bs4 import BeautifulSoup
+import requests
+import re
+import wget
+import os
+
+
+def send_request():
+    '''
+        input: None
+        output: None
+
+    '''
+    os.chdir('/home/clay/Downloads/stlCity')
+
+    source_files = "http://dynamic.stlouis-mo.gov/citydata/downloads/"
+
+    print '\nScraping ' + source_files + ' for .zip files'
+    resp = requests.get(source_files)
+    encoding = resp.encoding if 'charset' in resp.headers.get('content-type', '').lower() else None
+    soup = BeautifulSoup(resp.content, from_encoding=encoding)
+    os.chdir('zipFiles')
+    for link in soup.find_all('a', href=re.compile("\.zip")):
+        download_link = source_files + link['href']
+        if os.path.isfile(link['href']) == False:
+            print '\nDownloading ' + link['href']
+            wget.download(download_link)
+
+    print '\nAll *.zip files downloaded from ' + source_files
+    os.chdir('..')
+
+
+def main():
+    '''
+        Execute Main
+
+    '''
+
+    print 'executing request'
+    send_request()
+
+    print 'request complete!'
+
+
+if __name__ == '__main__':
+    main()
