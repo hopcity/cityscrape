d4defb951e67cf5493535e349e10bd7286b0dfca
= <claytoneyoung@gmail.com>
2015-07-26 20:45:57 -0500

    Initial commit 	new file:   AccessDump.py 	new file:   GrabAllFiles.py 	new file:   run_all.sh

---
+++ run_all.sh
@@ -0,0 +1,40 @@
+#!/bin/bash -e
+
+#	Dowload city access databases and shapes
+./GrabAllFiles.py
+
+#	Unzip Files
+cd zipFiles
+unzip -f "*.zip"
+
+
+for f in *.shp
+do
+	echo "Loading "$f
+	ogr2ogr -overwrite -progress -skipfailures -f "PostgreSQL" PG:"host=localhost user=postgres dbname=city" $f#
+	#shp2pgsql -I -s 6269 $f | psql -h localhost -U postgres -d city
+done
+
+
+for f in *.mdb
+do
+	echo "Extracting tables from $f"
+	#./../AccessDump.py $f
+
+	mdb-schema $f postgres | sed 's/Char/Varchar/g' | sed 's/Postgres_Unknown 0x0c/text/g' | psql -h localhost -U postgres -d city #> /dev/null 2>&1
+ 
+	tables=$(echo -en $(mdb-schema $f postgres | grep "CREATE TABLE" | awk '{ print $3 }' | sed -e 's/"//g');)
+	for i in $tables
+	do
+		echo "[File: "$f" ]	[Table - "$i"]"
+		#mdb-export -D '%%Y-%%m-%%d %%H:%%M:%%S' -q -I postgres $f $i | psql -h localhost -U postgres -d city #> /dev/null 2>&1
+		mdb-export -D ‘%%Y-%%m-%%d %%H:%%M:%%S’ -I postgress -q \’ -R \; $f $i | psql -d city -U postgres -w -h localhost
+#		 mdb-export -D '%%Y-%%m-%%d %%H:%%M:%%S' -Q $f $i > "$i.csv"
+		#mdb-export -I postgres -q \' $f $i | psql -d city -U postgres -w -h localhost
+
+	done
+
+done
+
+
+
