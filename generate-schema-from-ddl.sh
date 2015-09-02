204ad8aef377f2f738d2906b2af4a219de5b61e8
Dylan Raithel <dylanraithel@gmail.com>
2015-09-01 21:31:38 -0700

    accesses and runs file, needs type marshalling before succesful

--- generate-schema-from-ddl.sh
+++ generate-schema-from-ddl.sh
@@ -4,15 +4,27 @@ CONFIGFILE="config/cityscrape-config.sh"
 
 pushd $DDL_FILES
 
-files=$(echo `ls *.mdb 2>/dev/null`)
+mdb_files=$(echo `ls *.mdb 2>/dev/null`)
 
-if [[ -z "$files" ]]; then
-  echo "No Schema Definitions Found, Exiting..."
+if [[ -z "$mdb_files" ]]; then
+  echo "No MDB Schema Definitions Found, Exiting..."
   exit 3
 else
-  for file in $files
+  for mdb in $mdb_files
     do
-      cat $file | tr -d "[]" > $file.sql
+      cat $mdb | tr -d "[]" > $mdb.sql
+    done
+fi
+
+sql_files=$(echo `ls *.sql 2>/dev/null`)
+if [[ -z "$sql_files" ]]; then
+  echo "No Postgres Schema DDL, Exiting..."
+  exit 3
+else
+  for sql in $sql_files
+    do
+      psql -U vagrant -d city -a -w -f $sql
+      echo "[File: $sql] Completed Succesfully"
     done
 fi
 
